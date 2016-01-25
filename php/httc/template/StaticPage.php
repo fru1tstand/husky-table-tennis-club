1b037e1a48a45404fbf050974c336e4d1c58b123
Kodlee Yin <kodlee@kodleeshare.net>
2016-01-25 01:44:15 -0800

    Added EmptyStaticPage template

--- php/httc/template/StaticPage.php
+++ php/httc/template/StaticPage.php
@@ -4,25 +4,14 @@ require_once PHP_ROOT . '/httc/Setup.php';
 use common\template\component\TemplateField;
 use common\template\Content;
 /**
- * CSGO Derank template
+ * A static page with default banners and navigation links
  */
 class StaticPage extends Content {
 	const FIELD_BODY = "body";
 	const FIELD_TITLE = "title";
 
 	public static function getTemplateRenderContents(array $fields): string {
-		return <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Derank.Us {$fields[self::FIELD_TITLE]}</title>
-	<meta charset="UTF-8" />
-	<!--<link rel="shortcut icon" href="https://s3.amazonaws.com/ks_web/fru1t.me/favicon.ico" />-->
-	<!--<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway:400,600'>-->
-	<link rel="stylesheet" href="/styles/cache/raleway.css" />
-	<link rel="stylesheet" href="/styles/global.css" />
-</head>
-<body>
+		$fields[self::FIELD_BODY] = <<<HTML
 	<div id="content-wrapper">
 		<nav>
 		</nav>
@@ -34,9 +23,9 @@ class StaticPage extends Content {
 	</footer>
 
 	<!--<script src="/.site/js/analytics.js"></script>-->
-</body>
-</html>
 HTML;
+
+		return EmptyStaticPage::getTemplateRenderContents($fields);
 	}
 
 	public static function getTemplateFields_Internal(): array {
