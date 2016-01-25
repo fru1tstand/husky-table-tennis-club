1dd34d42f5ba9cfd8db0c68d860b850c4b6594e9
Kodlee Yin <kodlee@kodleeshare.net>
2016-01-25 01:44:34 -0800

    Started landing page layout

--- content/index.php
+++ content/index.php
@@ -1,12 +1,39 @@
 <?php
 require_once PHP_ROOT . '/httc/Setup.php';
-use httc\template\StaticPage;
+use httc\template\EmptyStaticPage;
 
 $body = <<<HTML
-hi
+<section class="front-page-banner">
+	<div>Husky Table Tennis Club</div>
+</section>
+<section class="front-tri">
+	<fieldset class="col-4">
+		<legend>About</legend>
+		<p>Welcome to the University of Washington's Husky Table Tennis Club. Connect with us on
+		<a href="https://www.facebook.com/groups/905310902883752/" target="_blank">Facebook</a> and
+		check us out on the <a href="https://www.youtube.com/channel/UCkTNmEuEjjfCAkJsrKQaGlg" target="_blank">YouTubes</a></p>
+	</fieldset>
+
+	<fieldset class="col-4">
+		<legend>Location</legend>
+		<p>Find us at <a href="https://www.google.com/maps/place/University+of+Washington+Intramural+Activities+Bldg,+3924+Montlake+Blvd+NE,+Seattle,+WA+98195/@47.6533996,-122.3035942,15.89z" target="_blank">IMA Gym B</a></p>
+		<div>&nbsp;</div>
+		<p>
+			3924 Montlake Blvd NE<br />
+			Seattle, WA 98195
+		</p>
+	</fieldset>
+
+	<fieldset class="col-4">
+		<legend>Hours</legend>
+		hi how are you
+	</fieldset>
+
+</section>
+asdfasdf
 HTML;
 
-StaticPage::createContent()
-		->with(StaticPage::FIELD_TITLE, "Home")
-		->with(StaticPage::FIELD_BODY, $body)
+EmptyStaticPage::createContent()
+		->with(EmptyStaticPage::FIELD_TITLE, "Home")
+		->with(EmptyStaticPage::FIELD_BODY, $body)
 		->render();
