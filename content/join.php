c23ef424b4f339185da4bb51eb5d2ddae3e5af52
fru1tstand <kodlee@fru1t.me>
2021-04-18 18:47:16 -0700

    Updated joining page to include interest form, waiver, hours, and price

--- content/join.php
+++ content/join.php
@@ -8,6 +8,11 @@ $body = <<<HTML
 	<div class="container">
 		<p>Players are welcome to drop in and bring friends to play during all open hours. A valid husky
 			card is required for membership, otherwise a per-session fee will apply.</p>
+		<br />
+		<p>Before arriving, please fill out
+		    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfrHtqkHdI0sH3BIV5_elf0uzat1Df90UzuszkAdgfiazfaUQ/viewform" target="_blank" class="link">this interest form</a>
+		    <b>AND</b>
+		    <a href="https://reg.recreation.uw.edu/Program/GetProgramDetails?courseId=5aadc2ad-34da-4b62-b725-f3ea15f74a5d&semesterId=fc4d5b28-d83f-424d-9a2b-d447bb7e8a0f" target="_blank" class="link">this club participation waiver</a>.</p>
 	</div>
 </div>
 
@@ -21,6 +26,7 @@ $body = <<<HTML
 				<li>Seattle, WA 98195</li>
 			</ul>
 		</a>
+		<br />
 		<p class="spaced-top">Gym B is in the back-left side of the building (upon entering).</p>
 	</div>
 </div>
@@ -28,9 +34,10 @@ $body = <<<HTML
 	<div class="container">
 		<div class="subsection-title">Hours<i class="fa fa-calendar"></i></div>
 		<ul class="no-list">
-			<li><span class="hours-dow">Monday</span>7pm-10pm</li>
-			<li><span class="hours-dow">Thursday</span>7pm-10pm</li>
+			<li><span class="hours-dow">Monday</span>4pm-7pm</li>
+			<li><span class="hours-dow">Thursday</span>4pm-7pm</li>
 		</ul>
+		<br />
 		<p class="spaced-top">All other times, the gym is open for badminton or basketball.</p>
 	</div>
 </div>
@@ -38,10 +45,11 @@ $body = <<<HTML
 	<div class="container">
 		<div class="subsection-title">Price<i class="fa fa-usd"></i></div>
 		<ul class="no-list">
-			<li><span class="hours-dow">Per Session</span>$1/person</li>
-			<li><span class="hours-dow">Per Quarter</span>$15/student</li>
-			<li><span class="hours-dow"></span>$25/adult</li>
+			<li><span class="hours-dow">First Time</span>FREE</li>
+			<li><span class="hours-dow">Per Quarter</span>$25</li>
+			<li><span class="hours-dow">Per Year</span>$60</li>
 		</ul>
+		<br />
 		<p class="spaced-top">As much as we love free stuff as well, the club purchases all the
 			equipment (balls, tables, nets, etc) out of pocket, so these fees help offset that cost.</p>
 	</div>
